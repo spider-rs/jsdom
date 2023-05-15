fb7ecb2cca6a1a3852758bda4d332f38e3ae7c0b
j-mendez <jeff@a11ywatch.com>
2023-05-15 11:13:29 -0400

    feat(tokio): enable tokio streaming support

--- src/extract.rs
+++ src/extract.rs
@@ -4,11 +4,11 @@ use ressa::*;
 #[cfg(not(feature = "hashbrown"))]
 use std::collections::HashSet;
 
-
 #[cfg(feature = "hashbrown")]
 use hashbrown::HashSet;
 
 /// extract all links that are created from a js script
+#[cfg(not(feature = "tokio"))]
 pub fn extract_links<T: PartialEq + Eq + std::hash::Hash + From<String>>(
     script: &str,
 ) -> HashSet<T> {
@@ -21,22 +21,22 @@ pub fn extract_links<T: PartialEq + Eq + std::hash::Hash + From<String>>(
                     Ok(program) => {
                         // entry script program
                         match program {
-                            ProgramPart::Decl(Decl::Var(VarKind::Var, _var_decl)) => {
-                                // for variable in var_decl {
-                                //     // start of variables capture into tree backed collection
-                                //     match variable.init {
-                                //         indent => match indent {
-                                //             Some(exp) => match exp {
-                                //                 Expr::Array(_) => {
-                                //                 }
-                                //                 _ => (),
-                                //             },
-                                //             _ => (),
-                                //         },
-                                //     }
-                                // }
-                            }
-                            ProgramPart::Decl(Decl::Class(_)) => {}
+                            // ProgramPart::Decl(Decl::Var(VarKind::Var, _var_decl)) => {
+                            //     for variable in var_decl {
+                            //         // start of variables capture into tree backed collection
+                            //         match variable.init {
+                            //             indent => match indent {
+                            //                 Some(exp) => match exp {
+                            //                     Expr::Array(_) => {
+                            //                     }
+                            //                     _ => (),
+                            //                 },
+                            //                 _ => (),
+                            //             },
+                            //         }
+                            //     }
+                            // }
+                            // ProgramPart::Decl(Decl::Class(_)) => {}
                             ProgramPart::Stmt(stmt) => {
                                 match stmt {
                                     Stmt::Expr(expression) => {
@@ -112,6 +112,123 @@ pub fn extract_links<T: PartialEq + Eq + std::hash::Hash + From<String>>(
                     _ => (),
                 }
             }
+        }
+        _ => (),
+    };
+
+    map
+}
+
+/// extract all links that are created from a js script
+#[cfg(feature = "tokio")]
+pub async fn extract_links<T: PartialEq + Eq + std::hash::Hash + From<String>>(
+    script: &str,
+) -> HashSet<T> {
+    use tokio_stream::StreamExt;
+    let mut map = HashSet::new();
+
+    match Parser::new(&Box::new(script)) {
+        Ok(p) => {
+            tokio::task::yield_now().await;
+            let mut stream = tokio_stream::iter(p);
+
+            while let Some(part) = stream.next().await {
+                match part {
+                    Ok(program) => {
+                        // entry script program
+                        match program {
+                            // ProgramPart::Decl(Decl::Var(VarKind::Var, _var_decl)) => {
+                            //     for variable in var_decl {
+                            //         // start of variables capture into tree backed collection
+                            //         match variable.init {
+                            //             indent => match indent {
+                            //                 Some(exp) => match exp {
+                            //                     Expr::Array(_) => {
+                            //                     }
+                            //                     _ => (),
+                            //                 },
+                            //                 _ => (),
+                            //             },
+                            //         }
+                            //     }
+                            // }
+                            // ProgramPart::Decl(Decl::Class(_)) => {}
+                            ProgramPart::Stmt(stmt) => {
+                                match stmt {
+                                    Stmt::Expr(expression) => {
+                                        match expression {
+                                            resast::expr::Expr::Assign(assign) => {
+                                                match assign {
+                                                    AssignExpr {
+                                                        operator,
+                                                        left,
+                                                        right,
+                                                    } => {
+                                                        // assignment to create node
+                                                        if operator == AssignOp::Equal {
+                                                            match left {
+                                                                AssignLeft::Expr(exp) => {
+                                                                    match *exp {
+                                                                        Expr::Ident(_idt) => {}
+                                                                        Expr::Member(mexp) => {
+                                                                            match mexp {
+                                                                                MemberExpr {
+                                                                                    object: _,
+                                                                                    property,
+                                                                                    computed,
+                                                                                } => {
+                                                                                    if !computed {
+                                                                                        match *property {
+                                                                                            Expr::Ident(
+                                                                                                ident,
+                                                                                            ) => {
+                                                                                                // links
+                                                                                                if ident.name == "href" {
+                                                                                                    match *right {
+                                                                                                        Expr::Lit(value) => {
+                                                                                                            match value {
+                                                                                                                Lit::String(string_lit) => {
+                                                                                                                    match string_lit {
+                                                                                                                        StringLit::Double(value) | StringLit::Single(value) => {
+                                                                                                                            map.insert(value.to_string().into());
+                                                                                                                        }
+                                                                                                                    }
+                                                                                                                }
+                                                                                                                _ => ()
+                                                                                                            }
+                                                                                                        },
+                                                                                                        _ => ()
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                            _ => (),
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                        _ => (),
+                                                                    }
+                                                                }
+                                                                _ => (),
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                            _ => (),
+                                        }
+                                    }
+                                    _ => (),
+                                }
+                            }
+                            _ => (),
+                        }
+                    }
+                    _ => (),
+                }
+            }
+
         }
         _ => (),
     };
--- tests/tests.rs
+++ tests/tests.rs
@@ -6,7 +6,7 @@ ele.href = 'https://a11ywatch.com';
 "###;
 
 #[test]
-#[cfg(not(feature = "hashbrown"))]
+#[cfg(all(not(feature = "hashbrown"), not(feature = "tokio")))]
 fn parse_links() {
     use std::collections::HashSet;
     // build tree with elements created from the nodes todo
@@ -16,11 +16,31 @@ fn parse_links() {
 }
 
 #[test]
-#[cfg(feature = "hashbrown")]
+#[cfg(all(feature = "hashbrown", not(feature = "tokio")))]
 fn parse_links() {
     use hashbrown::HashSet;
     // build tree with elements created from the nodes todo
     let links: HashSet<String> = extract_links(SCRIPT);
+
+    assert!(links.contains("https://a11ywatch.com"))
+}
+
+#[cfg(all(feature = "tokio", not(feature = "hashbrown")))]
+#[tokio::test]
+async fn parse_links() {
+    use std::collections::HashSet;
+    // build tree with elements created from the nodes todo
+    let links: HashSet<String> = extract_links(SCRIPT).await;
+
+    assert!(links.contains("https://a11ywatch.com"))
+}
+
+#[cfg(all(feature = "tokio", feature = "hashbrown"))]
+#[tokio::test]
+async fn parse_links() {
+    use hashbrown::HashSet;
+    // build tree with elements created from the nodes todo
+    let links: HashSet<String> = extract_links(SCRIPT).await;
 
     assert!(links.contains("https://a11ywatch.com"))
 }
